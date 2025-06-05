60f60813796ee1eda4321fc2a1835235db089e50
weolar <weolar@qq.com>
2025-06-05 11:23:10 +0800

    * 修复http://bbb.com/xxx/xxx.html 这类型url获取到的文件名不对的问题

--- mbvip/common/UrlUtil.h
+++ mbvip/common/UrlUtil.h
@@ -1,32 +1,53 @@
 
-#ifndef common_UrlUtil_h
-#define common_UrlUtil_h
+#ifndef content_UrlUtil_h
+#define content_UrlUtil_h
 
-#include "common/StringUtil.h"
+#include "content/common/StringUtil.h"
+#include "content/common/mbchar.h"
+
+namespace content {
 
 class UrlUtil {
 public:
+    // 从后往前找这几个字符串
+    static int backwardSearch(const std::string& input)
+    {
+        std::string targets[] = { "\\", "//", "/", "%2f", "%2F", "%5c", "%5C" };
+        for (int i = input.length() - 1; i >= 0; --i) {
+            for (const auto& target : targets) {
+                if (i >= static_cast<int>(target.length()) - 1) {
+                    std::string substr = input.substr(i - target.length() + 1, target.length());
+                    if (substr == target) {
+                        return i + 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
     // "1234-sss.ss?sz"  -> "1234-sss.ss"
     // "1234-sss.ss/" -> "1234-sss.ss"
-    static std::wstring getSaveNameFromUrl(const std::string& url)
+    static std::u16string getSaveNameFromUrl(const std::string& url)
     {
         if (0 == url.size())
-            return L"";
+            return std::u16string((const char16_t*)mbu16(""));
 
-        size_t pos1 = url.find_last_of('\\');
-        if (std::string::npos == pos1)
-            pos1 = url.size() - 1;
-        else
-            pos1++;
-
-        size_t pos2 = url.find_last_of('/');
-        if (std::string::npos == pos2)
-            pos2 = url.size() - 1;
-        else
-            pos2++;
-
-        size_t pos = pos1 < pos2 ? pos1 : pos2;
-        if (std::string::npos == pos || url.size() - 1 == pos)
+        //         size_t pos1 = url.find_last_of('\\');
+        //         if (std::string::npos == pos1)
+        //             pos1 = url.size() - 1;
+        //         else
+        //             pos1++;
+        // 
+        //         size_t pos2 = url.find_last_of('/');
+        //         if (std::string::npos == pos2)
+        //             pos2 = url.size() - 1;
+        //         else
+        //             pos2++;
+        // 
+        //         size_t pos = pos1 < pos2 ? pos1 : pos2;
+        int pos = backwardSearch(url);
+        if (-1 == pos || ((int)(url.size() - 1) == pos))
             pos = 0;
 
         size_t pos3 = url.find('?', pos);
@@ -36,8 +57,10 @@ public:
         if (pos3 < pos)
             pos3 = url.size();
         std::string path = url.substr(pos, pos3 - pos);
-        return common::utf8ToUtf16(path);
+        return utf8ToUtf16(path);
     }
 };
 
-#endif // common_UrlUtil_h
+}
+
+#endif // content_UrlUtil_h
